d9eb8a53abc0ec038c232949aae4d9bb95560927
Arturo de Jesús Rodríguez Arteaga <arturo.rdz3000@gmail.com>
2014-01-11 18:40:33 -0600

    Player basic movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 p = transform.position;
+		Vector3 mouseWorld = Camera.mainCamera.ScreenToWorldPoint(Input.mousePosition);
+		p.x = mouseWorld.x;
+		transform.position = p;
+	}
+}

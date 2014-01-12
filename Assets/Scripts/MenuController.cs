2ab0289a1016dedb64649a15a106d2e100f6ec99
Arturo de Jesús Rodríguez Arteaga <arturo.rdz3000@gmail.com>
2014-01-11 18:16:13 -0600

    Main Menu created and transition to Main Game

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public KeyCode startKey;
+	public Transform startButton;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		Debug.Log ("Running main level");
+		Application.LoadLevel ("main-game");
+	}
+}
